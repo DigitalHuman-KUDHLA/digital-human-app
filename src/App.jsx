e6a70171ae086cd67f35d02237626bd681ee24ea
satsuki2200 <132316826+satsuki2200@users.noreply.github.com>
2024-08-12 07:57:24 +0900

    忘れてた

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ function App() {
    */
   const imageFetch = () => {
     fetch('https://dog.ceo/api/breeds/image/random', {method: 'GET'}) 
+    // fetch('https://oaidalleapiprodscus.blob.core.windows.net/private/org-JXg8cxNwWXYaOmWgrCXZa7gv/user-qkHRKuvi2dYZRyS3dcD5WgkL/img-t0JAUAah3vMshfqnTXxLJAz8.png?st=2024-08-05T10%3A01%3A27Z&se=2024-08-05T12%3A01%3A27Z&sp=r&sv=2023-11-03&sr=b&rscd=inline&rsct=image/png&skoid=6aaadede-4fb3-4698-a8f6-684d7786b067&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2024-08-05T04%3A31%3A10Z&ske=2024-08-06T04%3A31%3A10Z&sks=b&skv=2023-11-03&sig=6ZyQGGLzNlMLhBc4p/q0/erhqc7MmcunjCLNmVwvZ5I%3D', {method: 'GET'})
     .then((response) => response.json())
     .then((data) => {
       setImage(data.message)
